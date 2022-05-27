f97ccf24bd6d5edd210f63c708f6011e0e310e6a
Martin <martin@marbl.nl>
2022-05-27 15:07:02 +0200

    add server-brand
    add server-brand

--- marbl_demo_addon/__manifest__.py
+++ marbl_demo_addon/__manifest__.py
@@ -26,7 +26,7 @@
         # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
-        'data/contact_data.xml'
+        'data/crm_data.xml'
     ],
     # only loaded in demonstration mode
     'demo': [
